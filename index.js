4f3ae3dca15bafba5a70e9c5c204b344e9091a83
Ajay Kumar <arvindkumar20102000@gmail.com>
2024-12-22 21:31:01 +0530

    committed

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import express from'express'
-//import colors from'colors'
+import colors from'colors'
 import dotenv from 'dotenv'
 //import morgan from 'morgan'
 import connectDb from './dbconfig/db.js';
-//import authRoutes from'./routes/authRoute.js'
-//import categoryRoutes from './routes/categoryRoute.js'
-//import productRoutes from './routes/productRoute.js'
+import authRoutes from'./routes/authRoute.js'
+import categoryRoutes from './routes/categoryRoute.js'
+import productRoutes from './routes/productRoute.js'
 import cors from 'cors'
 //import path from 'path'
 //import{fileURLToPath} from 'url'
@@ -24,14 +24,14 @@ const app=express()
 
 app.use(cors())
 app.use(express.json())
-// app.use(morgan('dev'))
-// app.use(express.static(path.join(__dirname,'./client/build')))
+ app.use(morgan('dev'))
+ //app.use(express.static(path.join(__dirname,'./client/build')))
 
-// app.use('/api/v1/auth',authRoutes)
+ app.use('/api/v1/auth',authRoutes)
 
-// app.use('/api/v1/category',categoryRoutes)
+ app.use('/api/v1/category',categoryRoutes)
 
-// app.use('/api/v1/product',productRoutes)
+ app.use('/api/v1/product',productRoutes)
 
 
 app.get('/',(req,res)=>{
