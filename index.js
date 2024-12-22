c57e180ace81881950b96ac62fff225abedb8401
Ajay Kumar <arvindkumar20102000@gmail.com>
2024-12-22 21:15:44 +0530

    committed

--- index.js
+++ index.js
@@ -9,39 +9,37 @@ import productRoutes from './routes/productRoute.js'
 import cors from 'cors'
 import path from 'path'
 import{fileURLToPath} from 'url'
-//config env
+
 dotenv.config();
 
-// //esmodule fix
-const __filename=fileURLToPath(import.meta.url)
-const __dirname=path.dirname(__filename)
 
-//db connection
+// const __filename=fileURLToPath(import.meta.url)
+// const __dirname=path.dirname(__filename)
+
+
 connectDb()
 
-//rest object
 const app=express()
 
-//middleware
+
 app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(express.static(path.join(__dirname,'./client/build')))
-//routes
-app.use('/api/v1/auth',authRoutes)
-//for category
-app.use('/api/v1/category',categoryRoutes)
-//for product
-app.use('/api/v1/product',productRoutes)
-//rest api
-//Port 
+
+// app.use('/api/v1/auth',authRoutes)
+
+// app.use('/api/v1/category',categoryRoutes)
+
+// app.use('/api/v1/product',productRoutes)
+
 
 app.get('/',(req,res)=>{
   res.send('deployed successfully')
 });
-app.use('*',function(req,res){
-res.sendFile(path.join(__dirname,'./client/build/index.html'))
-})
+// app.use('*',function(req,res){
+// res.sendFile(path.join(__dirname,'./client/build/index.html'))
+// })
 
 
 
