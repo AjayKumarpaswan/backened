c3c9271c7bf5fc2c3f317f9b9194db6353ae65a9
Ajay Kumar <arvindkumar20102000@gmail.com>
2024-12-22 21:27:47 +0530

    committed

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import express from'express'
-import colors from'colors'
+//import colors from'colors'
 import dotenv from 'dotenv'
-import morgan from 'morgan'
+//import morgan from 'morgan'
 import connectDb from './dbconfig/db.js';
-import authRoutes from'./routes/authRoute.js'
-import categoryRoutes from './routes/categoryRoute.js'
-import productRoutes from './routes/productRoute.js'
+//import authRoutes from'./routes/authRoute.js'
+//import categoryRoutes from './routes/categoryRoute.js'
+//import productRoutes from './routes/productRoute.js'
 import cors from 'cors'
-import path from 'path'
-import{fileURLToPath} from 'url'
+//import path from 'path'
+//import{fileURLToPath} from 'url'
 
 dotenv.config();
 
@@ -24,8 +24,8 @@ const app=express()
 
 app.use(cors())
 app.use(express.json())
-app.use(morgan('dev'))
-app.use(express.static(path.join(__dirname,'./client/build')))
+// app.use(morgan('dev'))
+// app.use(express.static(path.join(__dirname,'./client/build')))
 
 // app.use('/api/v1/auth',authRoutes)
 
